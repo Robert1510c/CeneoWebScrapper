cb0cad1eda56d6789c78a881df5b9f4c81d86da9
Robert1510c <robert1510c@gmail.com>
2023-05-31 19:30:50 +0200

    2023-05-31 templates RC

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,28 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Robert Ciepliński"
+    return render_template('glowna.html')
 
 @app.route('/name', defaults={'name': "Anonim"})
 @app.route('/name/<name>')
 def name(name=None):
     return f"Hello, {name}!"
+
+@app.route('/author')
+def author():
+    return render_template('autor.html')
+
+@app.route('/ekstrakcja')
+def ekstrakcja():
+    return render_template('ekstrakcja.html')
+
+@app.route('/lista_produktow')
+def listaproduktow():
+    return render_template('lista_produktow.html')
+
+@app.route('/produkt')
+def produkt():
+    return render_template('produkt.html')
